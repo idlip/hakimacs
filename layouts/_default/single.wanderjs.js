9207f2da846544c28f761d9d02f19cab2e29121e
Dilip <igoldlip@gmail.com>
2026-08-21 23:55:42 +0530

    init: whole html templates and layouts for theme

---
+++ layouts/_default/single.wanderjs.js
@@ -0,0 +1,12 @@
+{{- $page := site.GetPage "/wander" -}}
+{{- $dts := findRE `<dt>(?s:.*?)</dt>` $page.Content -}}
+{{- $consoles := split ($page.Params.wanderconsoles | default "") "|" -}}
+const wander = { consoles:
+[{{ range $consoles }}
+  {{ if . }}{{ (index (split . "::") 0) | jsonify }},{{ end }}
+{{ end }}],
+pages:
+[{{ range $i, $dt := $dts }}
+  {{ $url := $dt | replaceRE `(?s).*<a href="?([^ ">]+)"?.*` "$1" }}{{ $url | jsonify }},
+{{ end }}],
+styles: ["wander-theme.css"], ignore: [], }
