b96789d39e1d96c1dbe1748742137f9c61a42aad
Dilip <igoldlip@gmail.com>
2026-08-22 00:33:26 +0530

    fix(html): replace prettier-go with gotmplfm by hugo

--- shell.nix
+++ shell.nix
@@ -1,17 +1,8 @@
 with import <nixpkgs> {};
-let
-  # until: https://github.com/NixOS/nixpkgs/pull/551820/
-  prettierGoTemplate = pkgs.writeShellScriptBin "prettier-gotmpl" ''
-    exec ${pkgs.prettier}/bin/prettier \
-      --plugin=${pkgs.prettier-plugin-go-template}/lib/node_modules/prettier-plugin-go-template/lib/index.js \
-      "$@"
-  '';
-in
 pkgs.mkShell {
 
   buildInputs = with pkgs; [
     hugo just
-    treefmt pre-commit prettier taplo htmlhint
-    prettierGoTemplate
+    treefmt pre-commit prettier taplo htmlhint gotmplfmt
   ];
 }
